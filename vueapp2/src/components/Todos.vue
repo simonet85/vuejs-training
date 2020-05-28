a50ba8b94b9397a5de9f88da24ef43fd54d0ce19
simonet85 <christian.85@live.fr>
2020-05-28 10:08:20 +0200

    set up and first commit

---
+++ vueapp2/src/components/Todos.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="todos container">
+    <h1>Todos</h1>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <span :class="{ completed: todo.completed }">
+          <input type="checkbox" class="toggle" v-model="todo.completed" />
+          {{ todo.text }}
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todos",
+  data() {
+    return {
+      todos: [
+        {
+          text: "My Todo One",
+          completed: false
+        },
+        {
+          text: "My Todo Two",
+          completed: false
+        },
+        {
+          text: "My Todo Three",
+          completed: false
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+.container {
+  margin: auto;
+  width: 60%;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  padding: 10px 0;
+  border-bottom: 1px #ccc solid;
+}
+</style>
